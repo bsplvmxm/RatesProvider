8f5b07254752858acb86fe661080f28e307c1899
Timur <2001rich04@gmail.com>
2022-08-12 22:46:53 +0300

    add CurrencyRecipient

---
+++ RatesProvider/RatesProvider.Recipient/CurrencyRecipient.cs
@@ -0,0 +1,16 @@
+﻿using RatesProvider.Recipient.interfaces;
+
+namespace RatesProvider.Recipient;
+
+public class CurrencyRecipient : ICurrencyRecipient
+{
+    public string GetCurrencyPair()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string GetNeededCurruncy()
+    {
+        throw new NotImplementedException();
+    }
+}
