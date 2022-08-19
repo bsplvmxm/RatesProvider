ddb3f684fc3b2326245b0538ad084253aee3bccd
Maxim Bespalov <i.maksim_01@mail.ru>
2022-08-19 17:58:15 +0300

    edit code

--- RatesProvider/RatesProvider.Recipientt/CurrencyRecipient.cs
+++ RatesProvider/RatesProvider.Recipientt/CurrencyRecipient.cs
@@ -8,15 +8,12 @@ public class CurrencyRecipient : ICurrencyRecipient
     private static readonly HttpClient _httpClient = new HttpClient();
     public async Task<string> GetCurrencyPairFromPrimary(string neededCurrency)
     {
-        _httpClient.DefaultRequestHeaders.Accept.Clear();
-        _httpClient.DefaultRequestHeaders.Accept.Add(
-            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
         _httpClient.DefaultRequestHeaders.Add("apikey", "mbuKZX6GWcumRQ7KGgsw0FWAQ4IRiTmR");
 
-        var stringTask = _httpClient.GetStringAsync("https://api.apilayer.com/currency_data/live?source=USD&currencies=RUB,EUR,JPY,AMD,BGN,RSD");
-        var msg = await stringTask;
+        var stringCurrency = _httpClient.GetStringAsync("https://api.apilayer.com/currency_data/live?source=USD&currencies=RUB,EUR,JPY,AMD,BGN,RSD");
+        var currency = await stringCurrency;
 
-        return msg;
+        return currency;
     }
 
     public string GetCurrencyPairFromSecondary(string neededCurrency)
