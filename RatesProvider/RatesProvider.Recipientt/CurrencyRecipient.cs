804143d141648f586a5b24068b03ac4371e4e61a
Maxim Bespalov <i.maksim_01@mail.ru>
2022-08-19 18:07:03 +0300

    edit methods inc data

--- RatesProvider/RatesProvider.Recipientt/CurrencyRecipient.cs
+++ RatesProvider/RatesProvider.Recipientt/CurrencyRecipient.cs
@@ -1,4 +1,5 @@
-﻿using RatesProvider.Recipient.interfaces;
+﻿using RatesProvider.Recipient.Interfaces;
+using RatesProvider.Recipient.Enums;
 using System.Net.Http.Headers;
 
 namespace RatesProvider.Recipient;
@@ -6,7 +7,7 @@ namespace RatesProvider.Recipient;
 public class CurrencyRecipient : ICurrencyRecipient
 {
     private static readonly HttpClient _httpClient = new HttpClient();
-    public async Task<string> GetCurrencyPairFromPrimary(string neededCurrency)
+    public async Task<string> GetCurrencyPairFromPrimary(Rates source, string neededCurrency)
     {
         _httpClient.DefaultRequestHeaders.Add("apikey", "mbuKZX6GWcumRQ7KGgsw0FWAQ4IRiTmR");
 
@@ -16,7 +17,7 @@ public class CurrencyRecipient : ICurrencyRecipient
         return currency;
     }
 
-    public string GetCurrencyPairFromSecondary(string neededCurrency)
+    public async Task<string> GetCurrencyPairFromSecondary(Rates source, string neededCurrency)
     {
         throw new NotImplementedException();
     }
