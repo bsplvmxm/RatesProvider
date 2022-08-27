312b9d10b26beb6994f1412d7fc6516265c730f6
Timur <2001rich04@gmail.com>
2022-08-27 12:24:52 +0300

    Add envVars

---
+++ RatesProvider/RatesProvider.Recipientt/Infrastructure/EnvironmentVirable.cs
@@ -0,0 +1,8 @@
+﻿namespace RatesProvider.Recipient.Infrastructure;
+
+public static class EnvironmentVirable
+{
+    public static string PrimaryApiKey = "RATES_PROVIDER_PRIMARY_API_KEY";
+    public static string SecondaryApiKey = "RATES_PROVIDER_SECONDARY_API_KEY";
+    public static string Period = "RATES_PROVIDER_PERIOD";
+}
