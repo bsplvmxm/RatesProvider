63e0315493a11ef48aa942886c5e015769e07e63
Timur <2001rich04@gmail.com>
2022-08-31 18:10:26 +0300

    add logging

--- RatesProvider/RatesProvider.Handler/infrastructure/CurrencyHandler.cs
+++ RatesProvider/RatesProvider.Handler/infrastructure/CurrencyHandler.cs
@@ -3,6 +3,7 @@ using RatesProvider.Handler.Models;
 using RatesProvider.Recipient.Exceptions;
 using RatesProvider.Recipient.Interfaces;
 using System.Timers;
+using Microsoft.Extensions.Logging;
 
 namespace RatesProvider.Handler
 {
@@ -11,12 +12,14 @@ namespace RatesProvider.Handler
         private IRatesBuilder _modelBuilder;
         private IRatesGetter _currencyRecipient;
         private CurrencyRates _result;
+        private readonly ILogger _logger;
 
-        public CurrencyHandler(IRatesBuilder modelbuilder, IRatesGetter currencyRecipient)
+        public CurrencyHandler(IRatesBuilder modelbuilder, IRatesGetter currencyRecipient, ILogger logger)
         {
             _modelBuilder = modelbuilder;
             _currencyRecipient = currencyRecipient;
             _result = new CurrencyRates();
+            _logger = logger;
         }
 
         public async Task HandleAsync(object? sender, ElapsedEventArgs e)
@@ -24,22 +27,25 @@ namespace RatesProvider.Handler
             try
             {
                 // retry policy must be applied for both primary and secondary sources
+                _logger.LogInformation("Try handle primary api's response");
                 var passedCurrencyPairs = await _currencyRecipient.GetCurrencyPairFromPrimary();
                 _result.Rates = _modelBuilder.BuildPair<PrimaryRates>(passedCurrencyPairs).Quotes;
             }
             catch (RatesBuildException)
             {
+                _logger.LogInformation("Try handle secondary api's response");
                 var passedCurrencyPairs = await _currencyRecipient.GetCurrencyPairFromSecondary();
                 _result.Rates = _modelBuilder.ConvertToDecimal(_modelBuilder.BuildPair<SecondaryRates>(passedCurrencyPairs).Data);
             }
             catch (HttpRequestException)
             {
+                _logger.LogInformation("Try handle secondary api's response");
                 var passedCurrencyPairs = await _currencyRecipient.GetCurrencyPairFromSecondary();
                 _result.Rates = _modelBuilder.ConvertToDecimal(_modelBuilder.BuildPair<SecondaryRates>(passedCurrencyPairs).Data);
             }
             catch (Exception msg)
             {
-                Console.WriteLine(msg);
+                _logger.LogInformation("Unprocessable response: {0}", msg);
             }
         }
     }
